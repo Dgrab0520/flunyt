0c0db4bdc594cd67eb0687643e10e2e114837ff8
akftm <dgrab0520@gmail.com>
2021-10-18 16:38:21 +0900

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,6 @@ void main() {
   runApp(MyApp());
 }
 
-class MyCustomScrollBehavior extends MaterialScrollBehavior {
-  // Override behavior methods and getters like dragDevices
-  @override
-  Set<PointerDeviceKind> get dragDevices => {
-    PointerDeviceKind.touch,
-    PointerDeviceKind.mouse,
-    // etc.
-  };
-}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -4,191 +4,192 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 List<Widget> slide = [
-  SingleChildScrollView(
-
-    child: Column(
-      children: [
-        Container(
-          child: Image.asset("assets/lu_img12.jpg"),
-        ),
-        Row(
-          children: [
-            Expanded(flex:4,child: Container()),
-            Expanded(
-              flex:2,
-              child: InkWell(
-                onTap: (){
-                  print("pre order");
-                },
-                child: Container(
-                  padding: EdgeInsets.only(top:8),
-                  width:300,
-                  height:55,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color:Colors.white,
-                  ),
-                  child: Text('사전예약 하러하기',
-                    style: TextStyle(
-                      color:Color(0xFFa15c9b),
-                      fontSize:27,
-                      fontFamily: 'Jalnan',
+  ListView(
+    children: [
+      Column(
+        children: [
+          Container(
+            child: Image.asset("assets/lu_img12.jpg"),
+          ),
+          Row(
+            children: [
+              Expanded(flex:4,child: Container()),
+              Expanded(
+                flex:2,
+                child: InkWell(
+                  onTap: (){
+                    print("pre order");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.only(top:8),
+                    width:300,
+                    height:55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color:Colors.white,
                     ),
-                    textAlign: TextAlign.center,
+                    child: Text('사전예약 하러하기',
+                      style: TextStyle(
+                        color:Color(0xFFa15c9b),
+                        fontSize:27,
+                        fontFamily: 'Jalnan',
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(flex:4,child: Container()),
-          ],
-        ),
-        SizedBox(height:50),
-        Container(
-          child: Image.asset("assets/lu_img13.jpg"),
-        ),
-        SizedBox(width:30),
-        Container(
-          padding: EdgeInsets.only(top:30,bottom:30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Image.asset("assets/logo_W.png",width:150),
-
-                ],
-              ),
-              SizedBox(height:15),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('대표 | 이도원',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                  SizedBox(height:5),
-                  Text('주소 | 인천시 연수구 갯벌로 12 미주홀캠퍼스 별관 B동, 413,414호',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                  SizedBox(height:5),
-                  Text('문의 | dgrab0520@gmail.com',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                  SizedBox(height:5),
-                  Text('사업자 등록번호 | 110-45-71986',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                ],
-              ),
-
+              Expanded(flex:4,child: Container()),
             ],
           ),
-        ),
-      ],
-    ),
+          SizedBox(height:50),
+          Container(
+            child: Image.asset("assets/lu_img13.jpg"),
+          ),
+          SizedBox(width:30),
+          Container(
+            padding: EdgeInsets.only(top:30,bottom:30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Image.asset("assets/logo_W.png",width:150),
+
+                  ],
+                ),
+                SizedBox(height:15),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('대표 | 이도원',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text('주소 | 인천시 연수구 갯벌로 12 미주홀캠퍼스 별관 B동, 413,414호',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text('문의 | dgrab0520@gmail.com',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text('사업자 등록번호 | 110-45-71986',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ],
   ),
 
 ];
 
 List<Widget> slide2 = [
-  SingleChildScrollView(
-
-    child: Column(
-      children: [
-        Container(
-          child: Image.asset("assets/lu_img10.jpg"),
-        ),
-        InkWell(
-          onTap: (){
-            print("pre order");
-          },
-          child: Container(
-            padding: EdgeInsets.only(top:10),
-            width:240,
-            height:50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color:Colors.white,
-            ),
-            child: Text('사전예약 하러하기',
-              style: TextStyle(
-                color:Color(0xFFa15c9b),
-                fontSize:22,
-                fontFamily: 'Jalnan',
+  ListView(
+    children: [
+      Column(
+        children: [
+          Container(
+            child: Image.asset("assets/lu_img10.jpg"),
+          ),
+          InkWell(
+            onTap: (){
+              print("pre order");
+            },
+            child: Container(
+              padding: EdgeInsets.only(top:10),
+              width:240,
+              height:50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color:Colors.white,
               ),
-              textAlign: TextAlign.center,
+              child: Text('사전예약 하러하기',
+                style: TextStyle(
+                  color:Color(0xFFa15c9b),
+                  fontSize:22,
+                  fontFamily: 'Jalnan',
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
-        ),
-        SizedBox(height:50),
-        Container(
-          child: Image.asset("assets/lu_img11.jpg"),
-        ),
-        Container(
-          padding: EdgeInsets.only(top:30,bottom:30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Image.asset("assets/logo_W.png",width:120),
-
-                ],
-              ),
-              SizedBox(height:15),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('대표 | 이도원',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                  SizedBox(height:5),
-                  Text('주소 | 인천시 연수구 갯벌로 12 미주홀캠퍼스 별관 B동, 413,414호',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                  SizedBox(height:5),
-                  Text('문의 | dgrab0520@gmail.com',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                  SizedBox(height:5),
-                  Text('사업자 등록번호 | 110-45-71986',
-                    style:TextStyle(
-                      color:Colors.white,
-                      fontSize:12,
-                    ),
-                  ),
-                ],
-              ),
-
-            ],
+          SizedBox(height:50),
+          Container(
+            child: Image.asset("assets/lu_img11.jpg"),
           ),
-        ),
-      ],
-    ),
+          Container(
+            padding: EdgeInsets.only(top:30,bottom:30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Image.asset("assets/logo_W.png",width:120),
+
+                  ],
+                ),
+                SizedBox(height:15),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('대표 | 이도원',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text('주소 | 인천시 연수구 갯벌로 12 미주홀캠퍼스 별관 B동, 413,414호',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text('문의 | dgrab0520@gmail.com',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                    SizedBox(height:5),
+                    Text('사업자 등록번호 | 110-45-71986',
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontSize:12,
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    ],
   ),
 
 ];
